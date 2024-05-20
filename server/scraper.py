e0a637ee77888966f6deb9b57f08e425fa12e5ef
samippp <Sami.peng@hotmail.com>
2024-05-20 00:09:13 -0400

    scrapes 7274 anime in 154 secs

--- server/scraper.py
+++ server/scraper.py
@@ -43,22 +43,54 @@ def scrape_subpages(link):
 def scrape_season(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    all_anime = soup.select('div[class*="js-anime-category-producer seasonal-anime js-seasonal-anime js-anime-type-all js-anime-type-"]')
-    for x in all_anime:
-        score = x.find('div', title='Score')
-        popularity = x.find('div', class_='scormem-item member').text.strip()
-        name = x.find('a').text
-        genre_data = x.find_all('span', class_='genre')
-        genres = ''
-        studios = ''
-        for g in genre_data:
-            genres += g.findChildren()[0].text +','
-        genres = genres[0:len(genres)-1]
-        studios_data = x.find(lambda tag: tag.name == 'span' and 'Studio' in tag.text).next_siblings
-        for s in studios_data:
-            studios += s.text + ','
-        studios = studios[0:len(studios)-1]
-        print(name)
+    all_anime = soup.select('div[class*="seasonal-anime-list js-seasonal-anime-list js-seasonal-anime-list-key-"]')
+    
+    for anime in all_anime:
+        inidiv_anime = anime.select('div[class*="js-anime-type-"]')
+        for x in inidiv_anime:
+            img = ''
+            try:
+                img = x.find('img')['src']
+            except:
+                img = x.find('img')['data-src']
+            score = x.select('div[class*="scormem-item score score-label"]')[0].text.strip()
+            popularity = x.find('div', class_='scormem-item member').text.strip()
+            if 'K' not in popularity:
+                break
+            name = x.find('a').text
+            genre_data = x.find_all('span', class_='genre')
+            genres = ''
+            studios = ''
+            for g in genre_data:
+                genres += g.findChildren()[0].text +','
+            try:
+                themes = x.find(lambda tag: tag.name == 'span' and 'Theme' in tag.text).next_siblings
+                for t in themes:
+                    genres += t.text + ','
+            except:
+                pass
+            try:
+                demographic = x.find(lambda tag: tag.name == 'span' and 'Demographic' in tag.text).next_siblings
+                for d in demographic:
+                    genres += d.text + ','
+            except:
+                pass
+            genres = genres[0:len(genres)-1]
+            studios_data = x.find(lambda tag: tag.name == 'span' and 'Studio' in tag.text).next_siblings
+            for s in studios_data:
+                studios += s.text + ','
+            studios = studios[0:len(studios)-1]
+            dict = {
+                'name' : name,
+                'studios' : studios,
+                'genres' : genres,
+                'popularity' : popularity,
+                'score' : score,
+                'img' : img
+            }
+
+            with open("anime_data.json","a") as outfile:
+                json.dump(dict, outfile)
         
         
 
@@ -68,9 +100,13 @@ if __name__ == '__main__':
 
     if os.path.exists("anime_data.json"):
         os.remove("anime_data.json")
-
-    url = "https://myanimelist.net/anime/season"
-    scrape_season(url)
+    seasons = ['winter','spring','summer','fall']
+    for i in range(2004,2025):
+        for s in seasons:
+            url = 'https://myanimelist.net/anime/season/'+str(i)+'/'+s
+            scrape_season(url)
+    
+    
     
     '''scrape_subpages('https://myanimelist.net/anime/58447/Shibuya%E2%99%A1Hachi')'''
 
